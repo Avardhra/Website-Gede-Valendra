b367cc7b03b7dec65b74d0f12eec1e78f6820af5
Gede Valendra <gedevalendra738@gmail.com>
2025-10-05 18:02:54 +0700

    Update

--- src/Home/index.tsx
+++ src/Home/index.tsx
@@ -1,11 +1,11 @@
-import Gue from "../assets/img/gue.png";
+import gue from "../assets/img/gue.png";
 
 export default function Home() {
     return (
         <main className="position w-full min-h-[100vh] p-[3%] sm:px-[4%] md:px-[6%] lg:px-[10%] flex flex-col gap-4">
                 <div className="position mt-[6vh] w-full flex flex-col lg:flex-row justify-between items-center text-center lg:text-left">
                         <div className="lg:hidden relative w-full h-fit flex justify-center items-center">
-                                <img src="{Gue}" alt="Gede Valendra" className="w-[60%] md:w-48 md:h-48 lg:w-[50vh] lg:h-[50vh] bg-white rounded-full object-cover shadow-lg" />
+                                <img src={gue} alt="Gede Valendra" className="w-[60%] md:w-48 md:h-48 lg:w-[50vh] lg:h-[50vh] bg-white rounded-full object-cover shadow-lg" />
                                 <span className="absolute bottom-0 bg-white px-10 py-1 rounded-[100px] shadow-lg outline outline-1 outline-black/20">Programmer</span>
                         </div>
                         <div className="max-w-[100vh] w-full flex flex-col gap-5 items-center lg:items-start">
@@ -23,7 +23,7 @@ export default function Home() {
                                         </a>
                                 </div>
                         </div>
-                        <img src="{Gue}" alt="Gede Valendra" className="hidden lg:block w-32 h-32 md:w-48 md:h-48 lg:w-[50vh] lg:h-[50vh] bg-white rounded-bl-full rounded-tr-[40px] rounded-br-[5px] rounded-tl-[5px] object-cover shadow-lg" />
+                        <img src={gue} alt="Gede Valendra" className="hidden lg:block w-32 h-32 md:w-48 md:h-48 lg:w-[50vh] lg:h-[50vh] bg-white rounded-bl-full rounded-tr-[40px] rounded-br-[5px] rounded-tl-[5px] object-cover shadow-lg" />
                 </div>
         </main>
     );
